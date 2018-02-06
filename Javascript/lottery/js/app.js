58643a14b21e56b0990ff781a4b12feb2273a50f
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-02-06 22:18:31 +0200

    fix

--- Javascript/lottery/js/app.js
+++ Javascript/lottery/js/app.js
@@ -2,48 +2,42 @@
 
 function getResult(){
 
-var random = Math.floor(Math.random() * 10);
-var congratulations = 'Congratulations! You gassed the number! Number was';
-var lose = 'Sorry, try again! Number was';
-var message ='';
-var userValues = []; var txt = ' '; var resultText = '';
-
-var result = [];
-
-var select = document.getElementsByTagName('select');
-
- for(var i=0; i<3; i++){
-
- 	result[i] = Math.floor(Math.random() * 10);
- 	
- 	var e = select[i];
- 	
- 	userValues[i] = e.options[e.selectedIndex].value;
- 	
- 	if(userValues[i]==result[i]){
- 		alert('1 gassed number');
-
-	txt  = txt.concat(" ", result[i].toString());
-	resultText = congratulations + ' ' + txt;
-
-}
-else {
 	
-	txt  = txt.concat(" ",result[i].toString());
-	resultText = lose + ' ' + txt;
-}
+	var congratulations = 'Congratulations! You gassed the number! Number was', lose = 'Sorry, try again! Number was';
 
- }
+	var userValues = [], result = [];
+	var txt = ' ', resultText = '';
 
-/*
+	var yes=0;
 
+	var select = document.getElementsByTagName('select');
 
+	for(var i=0; i<select.length; i++){
 
-*/
+		result[i] = Math.floor(Math.random() * 10);
 
+		var e = select[i];
+
+		userValues[i] = e.options[e.selectedIndex].value;
+
+		if (userValues[i]==result[i]){
+			yes++;
+			
+		}
+			txt  = txt.concat(" ",result[i].toString());
+
+	}
+
+	if (yes!=0) {
+
+		resultText = congratulations + ' ' + txt;
+
+	}
+	else {
+		resultText = lose + ' ' + txt;
+
+	}
 
 document.getElementById('lotteryResult').innerHTML = resultText;
-
-
 
 }
