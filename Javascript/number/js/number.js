23151ed39e3e7c7ea72dfbee33fde4b029bd0eda
MargaritaLadybird <rita.kolosok@gmail.com>
2018-02-06 17:20:48 +0200

    isNaN(), Infinity

--- Javascript/number/js/number.js
+++ Javascript/number/js/number.js
@@ -1,1 +1,27 @@
 // number.js
+
+
+function checkInfinity(){
+	var infinityValue = document.getElementById('infinityValue').value;
+
+	var txt = '', answer= ''; var x;
+	console.log(isNaN(infinityValue));
+
+
+	if(isNaN(infinityValue)) {
+		
+		answer = 'Value is NAN';
+
+	}			
+	else {
+
+		while(infinityValue!= Infinity) {
+			infinityValue = infinityValue * infinityValue;
+			txt = txt + ' ' + infinityValue + "<br>";
+			answer = txt; }
+
+		}
+
+		document.getElementById('demo').innerHTML = answer;
+
+	}
