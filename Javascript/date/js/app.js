62d8222382a3e9870d00cec5e249cf403b7db12a
MargaritaLadybird <rita.kolosok@gmail.com>
2018-02-07 14:03:38 +0200

    Check weekday of your birth

--- Javascript/date/js/app.js
+++ Javascript/date/js/app.js
@@ -3,7 +3,37 @@ window.onload = function () {
 
 	var dataStr = new Date();
 	document.getElementById("demo").innerHTML = dataStr.toDateString();	
-	
+
 }
+
+
+function getDay(){
+	var txt = '';
+	var weekDay = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+		function checkDate(date){
+		if(date=='Invalid Date'){
+			
+			txt = 'Wrong date format! Check input and try again!';
+			
+			return true;
+		}
+	}
+
+	var date = document.getElementById("day").value;
+	date = new Date(date);
+
+	if(!checkDate(date)){
+
+		var day= date.getDay();
+		txt = weekDay[day];
+
+	}
+
+	document.getElementById('dayResult').innerHTML = txt;
+
+}
+
+
 
 
