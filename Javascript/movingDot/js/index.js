118bd75168fab0449cc280a9c8ca3069d20435d3
MargaritaLadybird <rita.kolosok@gmail.com>
2018-02-09 09:39:29 +0200

    moving dot

--- Javascript/movingDot/js/index.js
+++ Javascript/movingDot/js/index.js
@@ -1,1 +1,24 @@
 // app.js
+/*
+window.onload = function(){
+		var spanArray = '';
+		for(var i=0; i<5; i++){
+
+		spanArray+= '<span>' + i + '</span>';
+
+		}
+		document.getElementById('demo').innerHTML = spanArray;
+	}
+
+var myVar = setInterval(myTimer, 1000);
+var i=0;
+var arr = document.getElementById('demo').childNodes;
+
+function myTimer() {
+	
+	arr[i].classList.add('dot');
+	i++;
+	   
+
+}
+*/
