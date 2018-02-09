669427fb5dbac8867e709df604f224cfa1a5b316
MargaritaLadybird <rita.kolosok@gmail.com>
2018-02-09 11:28:54 +0200

    # Movin dor in 1 dimmension array

--- Javascript/movingDot/js/index.js
+++ Javascript/movingDot/js/index.js
@@ -38,3 +38,45 @@ window.onload = function(){
 
 
 */
+
+var node = document.getElementById('demo').childNodes;
+		var startPos = node[0].classList.add('dot');
+
+		
+		function moveRight(){
+			if(i==node.length-1){
+				node[i].classList.remove('dot');
+
+				i=0;
+				node[i].classList.add('dot');
+				
+			}
+			else {
+
+				i++;
+				
+				node[i].classList.add('dot');
+				node[i-1].classList.remove('dot');
+			} 
+
+			
+		}
+
+		function moveLeft(){
+			if(i==0){
+				
+				node[i].classList.remove('dot');
+				i=node.length -1;
+				node[i].classList.add('dot');
+
+			}
+			else {
+				node[i].classList.remove('dot');
+				i--;
+				node[i].classList.add('dot');
+				
+				console.log('ELSE' + i);
+
+			}
+			
+		}
