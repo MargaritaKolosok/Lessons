b5f2f6ff6b58b2a24aa4804e64995e17745fabd5
MargaritaLadybird <rita.kolosok@gmail.com>
2018-02-12 15:55:33 +0200

    #x

--- Javascript/movingDot/js/index.js
+++ Javascript/movingDot/js/index.js
@@ -1,44 +1,6 @@
 // app.js
+
 /*
-
-// Dot moving Interval
-
-window.onload = function(){
-			var demo = document.getElementById('demo');
-			var spanArray='';
-			for(var i=0; i<5; i++){
-
-				spanArray+= '<span>' + i + '</span>';
-			}
-			demo.innerHTML = spanArray;
-
-		}
-
-		var i=0;
-
-		var myTimer = setInterval(moveDot, 1000);
-		var arrSpan = demo.childNodes;
-
-		function moveDot(){
-			if(i!=arrSpan.length){
-
-				arrSpan[i].classList.toggle('dot');
-
-				console.log(i + 'if');
-
-				i++;
-
-			}
-			else {
-				i=0;
-				console.log(i + 'else');
-			} 
-
-		}
-
-
-*/
-
 var node = document.getElementById('demo').childNodes;
 		var startPos = node[0].classList.add('dot');
 
@@ -75,8 +37,8 @@ var node = document.getElementById('demo').childNodes;
 				i--;
 				node[i].classList.add('dot');
 				
-				console.log('ELSE' + i);
-
+				
 			}
 			
 		}
+*/
