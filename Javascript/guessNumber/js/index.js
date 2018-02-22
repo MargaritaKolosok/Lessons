16506ba583cdd25ca15e49fda50befac19b786a1
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-02-22 20:06:03 +0200

    guessNumber

--- Javascript/guessNumber/js/index.js
+++ Javascript/guessNumber/js/index.js
@@ -1,1 +1,21 @@
 // app.js
+
+/*
+
+                if(x!=0 || x!=numArr.length){
+                    if(numArr[x]==randomArr[x-1]) { result[x] = 'L'; }
+                    if(numArr[x]==randomArr[x+1]) { result[x] = 'R'; }
+
+                }
+                else if(x==0){
+                    if(numArr[x]==randomArr[numArr.length]) { result[x] = 'L'; }
+                    if(numArr[x]==randomArr[x+1]) { result[x] = 'R'; }
+
+                }
+                else if(x==numArr.length){
+                    if(numArr[x]==randomArr[0]) { result[x] = 'L'; }
+                    if(numArr[x]==randomArr[x-1]) { result[x] = 'R'; }
+
+                }
+
+*/
