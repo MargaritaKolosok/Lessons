d0e828ff53a2cd125d5e87639ce9a987832f03a6
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-03-21 20:27:35 +0200

    #2

--- selenium/QA.js
+++ selenium/QA.js
@@ -4,7 +4,7 @@ var By = webdriver.By;
 var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
 driver.get('https://vb-web-qas.azurewebsites.net/#/login').then(function(){
-    driver.findElement(By.id('email')).then(function(email){
+    driver.wait(until.elementLocated(By.id('email')).then(function(email){
         email.click();
         email.sendKeys('rita.kolosok@gmail.com');
         var password = driver.findElement(By.id('password'));
@@ -13,7 +13,7 @@ driver.get('https://vb-web-qas.azurewebsites.net/#/login').then(function(){
 
         var login = driver.findElement(By.css('.default vb-band-button'));
         login.click();
-    });
+    }));
 });
 
 
