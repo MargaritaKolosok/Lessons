ba4a2b38efd83aed55479bc486a76c7814d1a119
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-04-02 19:00:28 +0300

    class / object

---
+++ c#/batterfly/batterfly/Butterfly.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace batterfly
+{
+    class Butterfly
+    {
+        public string name;
+
+        public void fly() {
+            Console.WriteLine(name + " can fly");
+            Console.ReadKey();
+        }
+    }
+}
