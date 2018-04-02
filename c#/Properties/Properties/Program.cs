4ab42c4414ef177605163efb00e221607cc5b36e
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-04-02 20:31:47 +0300

    private / public

---
+++ c#/Properties/Properties/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Properties
+{
+    class MyClass
+    {
+
+        // закрытое строковое поле ссылочного типа
+
+        private string field = null;
+
+        public void SetField(string value) { // Method setter
+
+           field = value;
+
+        }
+
+        public string GetField() { // getter
+
+            return field;
+
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            MyClass instance = new MyClass();
+
+            instance.SetField("Hello World!");
+
+            string @string = instance.GetField();
+
+            Console.WriteLine(@string);
+            Console.ReadKey();
+
+        }
+    }
+}
