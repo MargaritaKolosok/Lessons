335830d4e0ce23e0eda99477c6294fc75057854e
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-04-03 15:58:36 +0300

    public / privat / if / else

---
+++ c#/Properties_04/Properties_04/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_04
+{
+    class MyClass {
+
+        private string field = null;
+
+        public string Field {
+
+            set {
+                if (value == "fool")
+                {
+                    Console.WriteLine("Wrong value entered, Try again!");
+                }
+                else {
+
+                    field = value;
+                    
+                }
+            }
+
+            get {
+
+                if (field == null)
+                    return "There is no data in field";
+                else if (field == "hello world")
+                    return field.ToUpper() + '!';
+                else
+                    return field;
+
+
+            }
+
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+
+            instance.Field = "fool";
+            Console.WriteLine(instance.Field);
+
+            Console.WriteLine(new string('-', 50));
+
+            instance.Field = "hello world";
+            Console.WriteLine(instance.Field);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
