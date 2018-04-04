e76f783783fe350c2505013433d960ef9df63593
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-04-04 21:45:55 +0300

    конструкторы
    онструктор по умолчанию, и пользовательский

---
+++ c#/Constructors_001/Constructors_001/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point pointA = new Point();
+
+            Console.WriteLine("X={0}, Y={1}", pointA.X, pointA.Y);
+
+            Console.WriteLine(new string('-', 50));
+
+            Point pointB = new Point(100, 200);
+            Console.WriteLine("X={0}, Y={1}", pointB.X, pointB.Y);
+
+            Console.ReadKey();
+
+        }
+    }
+}
