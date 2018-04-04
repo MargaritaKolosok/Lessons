e92839690a3ddbc465b9b211775d22277d042a5b
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-04-04 21:19:52 +0300

    get set error notification in Visual studio

---
+++ c#/Properties_005/Properties_005/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_005
+{
+    class Constants {
+
+        private double pi = 3.14D;
+        private double e = 2.71D;
+
+        public double Pi {
+            // Свойство только для записи
+            set {
+                pi = value;
+            }
+
+        }
+
+        public double E {
+            // Свойство только для считывания
+            get {
+                return e;
+            }
+
+
+        }
+
+     
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Constants constants = new Constants();
+            constants.Pi = 3.14989898989;
+            // lack of get accessor error
+            //  Console.WriteLine(constants.Pi); 
+            
+            // property is read only error
+            // constants.E = 2.87878778787;
+            Console.WriteLine("e = {0}", constants.E);
+
+            Console.ReadKey();
+        }
+    }
+}
