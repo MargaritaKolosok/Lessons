5205c414cc89570a7c362e0e3bf492eb7c1985ef
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-04-10 10:27:43 +0300

    C#

---
+++ c#/Properties_006/Properties_006/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_006
+{
+    class Point
+    {
+        private int x, y;
+
+        public int X
+        {
+            get { return x; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+
+        }
+        // Конструктор по умолчанию, инициализирующий поля значениями по умолчанию
+        public Point()
+        {
+            Console.WriteLine("Конструктор по умолчанию!");
+        }
+
+        // Пользовательский конструктор, инициализирующий поля значениями заданными пользователем
+
+        public Point(int x, int y)
+        {
+            Console.WriteLine("Пользовательский конструктор");
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+}
